cd8e7d519183946d26adf00f5421ad93d4705de3
cf7 <cfranco7@lion.lmu.edu>
2017-11-17 04:07:12 -0600

    custom quill toolbar with color picker, not done

---
+++ src/components/color.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SketchPicker } from 'react-color';
+
+export default class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: ""
+    };
+  }
+
+  handleChangeComplete(color) {
+    this.setState({
+      color: color.hex
+    });
+  }
+
+  render() {
+    return (
+        <SketchPicker
+              color={this.state.color}
+              onChangeComplete={this.handleChangeComplete} />
+    );
+  }
+}
+
--- src/components/editor.js
+++ src/components/editor.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Quill from 'react-quill';
+import ReactQuill, { Quill } from 'react-quill';
+import ColorPicker from './color';
+
+function pickColor (event) {
+  console.log(event.target);
+  // access ColorPicker
+}
+/*
+ * Custom toolbar component including ColorPicker button
+ */
+const CustomToolbar = () => (
+  <div id="toolbar">
+    <select className="ql-header">
+      <option value="1"></option>
+      <option value="2"></option>
+      <option value="3"></option>
+    </select>
+    <button className="ql-bold"></button>
+    <button className="ql-italic"></button>
+    <button className="ql-color">
+      <ColorPicker />
+    </button>
+  </div>
+);
 
 export default class Editor extends React.Component {
   constructor(props) {
@@ -8,6 +31,12 @@ export default class Editor extends React.Component {
     this.state = {
 
     };
+
+    // let Color = Quill.import('formats/color');
+    // // TODO: import color picker as react component
+    // // find way to swap out color picker on quill toolbar
+    // Quill.register(Color);
+
     let toolbarOptions = [
       ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
       ['blockquote', 'code-block'],
@@ -21,25 +50,35 @@ export default class Editor extends React.Component {
       [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
       [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'background': [] }],          // dropdown with defaults from theme
       [{ 'font': [] }],
       [{ 'align': [] }],
 
       ['clean']                                         // remove formatting button
     ];
     this.modules = {
-      toolbar: toolbarOptions,
+      toolbar: {
+        container: "#toolbar",
+        handlers: {
+          "color": pickColor,
+        }
+      }
     };
     this.formats = [
-
+      'header',
+      'bold', 'italic', 'underline', 'strike', 'blockquote',
+      'list', 'bullet', 'indent',
+      'link', 'image'
     ];
   }
 
   render() {
     return (
       <div className="text-editor">
-        <Quill
-            modules={this.modules} />
+        <CustomToolbar />
+        <ReactQuill
+            modules={this.modules}
+            formats={this.formats} />
       </div>
     );
   }
